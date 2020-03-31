dae3a5cff70ec1c36d93d9d22f790f7ce84f6966
mac <aelkhazenti@gmail.com>
2020-03-31 22:17:30 +0100

    add methode main

--- src/main.java
+++ src/main.java
@@ -1,2 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class main {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    calculator frame = new calculator ();
+                    frame.setVisible(true);
+
+
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
 }
